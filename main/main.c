a8b5a9d11670b8877f5269808291c0afe224b056
DAWN <limingmorning@163.com>
2023-08-18 13:28:51 +0800

    搞定wifi和ntp

--- main/main.c
+++ main/main.c
@@ -46,9 +46,9 @@ void time_sync_notification_cb(struct timeval *tv)
 
 static void obtain_time(void)
 {
-    ESP_ERROR_CHECK(nvs_flash_init());
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    // ESP_ERROR_CHECK(nvs_flash_init());
+    // ESP_ERROR_CHECK(esp_netif_init());
+    // ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     initialize_sntp();
 
@@ -317,172 +317,192 @@ void temp_hum_task(void *pvParameters)
     }
 }
 
-/* FreeRTOS event group to signal when we are connected & ready to make a request */
-static EventGroupHandle_t s_wifi_event_group;
+/* 宏定义WiFi名称和密码 */
+#define MY_WIFI_SSID    "MERCURY_9A5E"
+#define MY_WIFI_PASSWD  "11111111"
 
-/* The event group allows multiple bits for each event,
-   but we only care about one event - are we connected
-   to the AP with an IP? */
-static const int CONNECTED_BIT = BIT0;
-static const int ESPTOUCH_DONE_BIT = BIT1;
-static const char *TAG = "smartconfig_example";
+/* 宏定义WiFi连接事件标志位、连接失败标志位及智能配网标志位 */
+#define WIFI_CONNECTED_BIT  BIT0
+#define WIFI_FAIL_BIT       BIT1
+#define SMART_CONFIG_BIT    BIT2
 
-static void smartconfig_example_task(void *parm);
+/* 定义一个WiFi连接事件标志组句柄 */
+static EventGroupHandle_t wifi_event_group_handler;
 
-static void event_handler(void *arg, esp_event_base_t event_base,
-                          int32_t event_id, void *event_data)
+static void wifi_init_sta(void);
+/* 系统事件循环处理函数 */
+static void event_handler(void* arg, esp_event_base_t event_base,int32_t event_id, void* event_data)
 {
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+    static int retry_num = 0;           /* 记录wifi重连次数 */
+    /* 系统事件为WiFi事件 */
+    if (event_base == WIFI_EVENT)
     {
-        xTaskCreate(smartconfig_example_task, "smartconfig_example_task", 4096, NULL, 3, NULL);
+        if(event_id == WIFI_EVENT_STA_START)    /* 事件id为STA开始 */
+            esp_wifi_connect();
+        else if (event_id == WIFI_EVENT_STA_DISCONNECTED) /* 事件id为失去STA连接 */
+        {
+            esp_wifi_connect();
+            retry_num++;
+            printf("retry to connect to the AP %d times. \n",retry_num);
+            if (retry_num > 10)  /* WiFi重连次数大于10 */
+            {
+                /* 将WiFi连接事件标志组的WiFi连接失败事件位置1 */
+                xEventGroupSetBits(wifi_event_group_handler, WIFI_FAIL_BIT);
+            }
+            /* 清除WiFi连接成功标志位 */
+            xEventGroupClearBits(wifi_event_group_handler, WIFI_CONNECTED_BIT);
+        }
     }
-    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
-    {
-        esp_wifi_connect();
-        xEventGroupClearBits(s_wifi_event_group, CONNECTED_BIT);
-    }
+    /* 系统事件为ip地址事件，且事件id为成功获取ip地址 */
     else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
     {
-        xEventGroupSetBits(s_wifi_event_group, CONNECTED_BIT);
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data; /* 获取IP地址信息*/
+        printf("got ip:%d.%d.%d.%d \n" , IP2STR(&event->ip_info.ip));  /* 打印ip地址*/
+        retry_num = 0;                                              /* WiFi重连次数清零 */
+        /* 将WiFi连接事件标志组的WiFi连接成功事件位置1 */
+        xEventGroupSetBits(wifi_event_group_handler, WIFI_CONNECTED_BIT);
     }
-    else if (event_base == SC_EVENT && event_id == SC_EVENT_SCAN_DONE)
+    /* 系统事件为智能配网事件 */
+    else if (event_base == SC_EVENT)
     {
-        ESP_LOGI(TAG, "Scan done");
-    }
-    else if (event_base == SC_EVENT && event_id == SC_EVENT_FOUND_CHANNEL)
-    {
-        ESP_LOGI(TAG, "Found channel");
-    }
-    else if (event_base == SC_EVENT && event_id == SC_EVENT_GOT_SSID_PSWD)
-    {
-        ESP_LOGI(TAG, "Got SSID and password");
-
-        smartconfig_event_got_ssid_pswd_t *evt = (smartconfig_event_got_ssid_pswd_t *)event_data;
-        wifi_config_t wifi_config;
-        uint8_t ssid[33] = {0};
-        uint8_t password[65] = {0};
-        uint8_t rvd_data[33] = {0};
-
-        bzero(&wifi_config, sizeof(wifi_config_t));
-        memcpy(wifi_config.sta.ssid, evt->ssid, sizeof(wifi_config.sta.ssid));
-        memcpy(wifi_config.sta.password, evt->password, sizeof(wifi_config.sta.password));
-        wifi_config.sta.bssid_set = evt->bssid_set;
-        if (wifi_config.sta.bssid_set == true)
+        if(event_id == SC_EVENT_SCAN_DONE )             /* 开始扫描智能配网设备端 */
+            printf("Scan done\n");
+        else if(event_id == SC_EVENT_FOUND_CHANNEL)     /* 得到了智能配网通道 */
+            printf("Found channel \n");
+        else if(event_id == SC_EVENT_GOT_SSID_PSWD)     /* 得到了智能配网设备提供的ssid和password */
         {
-            memcpy(wifi_config.sta.bssid, evt->bssid, sizeof(wifi_config.sta.bssid));
-        }
+            printf("smartconfig got SSID and password\n");
+            /* 获取智能配网设备端提供的数据信息 */
+            smartconfig_event_got_ssid_pswd_t *evt = (smartconfig_event_got_ssid_pswd_t *)event_data;
+            /* 定义WiFi配置结构体和用了结收ssid和password的数组 */
+            wifi_config_t wifi_config;
+            uint8_t ssid[33] = { 0 };
+            uint8_t password[65] = { 0 };
 
-        memcpy(ssid, evt->ssid, sizeof(evt->ssid));
-        memcpy(password, evt->password, sizeof(evt->password));
-        ESP_LOGI(TAG, "SSID:%s", ssid);
-        ESP_LOGI(TAG, "PASSWORD:%s", password);
-        if (evt->type == SC_TYPE_ESPTOUCH_V2)
-        {
-            ESP_ERROR_CHECK(esp_smartconfig_get_rvd_data(rvd_data, sizeof(rvd_data)));
-            ESP_LOGI(TAG, "RVD_DATA:");
-            for (int i = 0; i < 33; i++)
-            {
-                printf("%02x ", rvd_data[i]);
+            bzero(&wifi_config, sizeof(wifi_config_t)); /* 将结构体数据清零 */
+            /* 将智能配网设备发送来的WiFi的ssid、password及MAC地址复制到wifi_config */
+            memcpy(wifi_config.sta.ssid, evt->ssid, sizeof(wifi_config.sta.ssid));
+            memcpy(wifi_config.sta.password, evt->password, sizeof(wifi_config.sta.password));
+            wifi_config.sta.bssid_set = evt->bssid_set;
+            if (wifi_config.sta.bssid_set == true) {
+                memcpy(wifi_config.sta.bssid, evt->bssid, sizeof(wifi_config.sta.bssid));
             }
-            printf("\n");
-        }
 
-        ESP_ERROR_CHECK(esp_wifi_disconnect());
-        ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
-        esp_wifi_connect();
-    }
-    else if (event_base == SC_EVENT && event_id == SC_EVENT_SEND_ACK_DONE)
-    {
-        xEventGroupSetBits(s_wifi_event_group, ESPTOUCH_DONE_BIT);
+            /* 打印WiFi名称和密码 */
+            memcpy(ssid, evt->ssid, sizeof(evt->ssid));
+            memcpy(password, evt->password, sizeof(evt->password));
+            printf("SSID:%s \n", ssid);
+            printf("PASSWORD:%s \n", password);
+
+            /* 根据得到的WiFi名称和密码连接WiFi*/
+            ESP_ERROR_CHECK( esp_wifi_disconnect() );
+            ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+            ESP_ERROR_CHECK( esp_wifi_connect() );
+        }
+        else if (event_id == SC_EVENT_SEND_ACK_DONE)    /* 智能配网成功，已经给智能配网设备发送应答*/
+        {
+            xEventGroupSetBits(wifi_event_group_handler, SMART_CONFIG_BIT);
+        }
     }
 }
 
-static void initialise_wifi(void)
+static void smartconfig_init_start(void)
 {
-    ESP_ERROR_CHECK(esp_netif_init());
-    s_wifi_event_group = xEventGroupCreate();
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
-    assert(sta_netif);
+    /* 设置智能配网类型为 AirKiss */
+    ESP_ERROR_CHECK( esp_smartconfig_set_type(SC_TYPE_AIRKISS) );
+    /* 通过SMARTCONFIG_START_CONFIG_DEFAULT宏 来获取一个默认的smartconfig配置参数结构体变量*/
+    smartconfig_start_config_t cfg = SMARTCONFIG_START_CONFIG_DEFAULT();
+    /* 开始智能配网 */
+    ESP_ERROR_CHECK( esp_smartconfig_start(&cfg) );
+    printf("smartconfig start ....... \n");
 
+    /* 使用事件标志组等待连接建立(WIFI_CONNECTED_BIT)或连接失败(WIFI_FAIL_BIT)事件 */
+    EventBits_t uxBits;  /* 定义一个事件位变量来接收事件标志组等待函数的返回值 */
+    /* 等待事件标志组，退出前清除设置的事件标志，任意置1就会返回*/
+    uxBits = xEventGroupWaitBits(wifi_event_group_handler, WIFI_CONNECTED_BIT | SMART_CONFIG_BIT,
+                                    true, false, portMAX_DELAY);
+    if(uxBits & WIFI_CONNECTED_BIT)
+    {
+        printf("WiFi Connected to ap ok. \n");
+    }
+    if(uxBits & SMART_CONFIG_BIT)
+    {
+        printf("smartconfig over \n");
+        esp_smartconfig_stop(); /* 关闭智能配网 */
+    }
+}
+
+
+void wifi_init_sta(void)
+{
+    /* 创建一个事件标志组 */
+    wifi_event_group_handler = xEventGroupCreate();
+
+    /* 初始化底层TCP/IP堆栈。在应用程序启动时，应该调用此函数一次。*/
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    /* 创建默认事件循环,*/
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* 创建一个默认的WIFI-STA网络接口，如果初始化错误，此API将中止。*/
+    esp_netif_create_default_wifi_sta();
+
+    /* 使用WIFI_INIT_CONFIG_DEFAULT() 来获取一个默认的wifi配置参数结构体变量*/
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    /* 根据cfg参数初始化wifi连接所需要的资源 */
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
+	/* 将事件处理程序注册到系统默认事件循环，分别是WiFi事件、IP地址事件及smartconfig事件 */
     ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
     ESP_ERROR_CHECK(esp_event_handler_register(SC_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
 
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    ESP_ERROR_CHECK(esp_wifi_start());
-}
-
-static void smartconfig_example_task(void *parm)
-{
-    EventBits_t uxBits;
-    ESP_ERROR_CHECK(esp_smartconfig_set_type(SC_TYPE_ESPTOUCH));
-    smartconfig_start_config_t cfg = SMARTCONFIG_START_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_smartconfig_start(&cfg));
-    while (1)
-    {
-        uxBits = xEventGroupWaitBits(s_wifi_event_group, CONNECTED_BIT | ESPTOUCH_DONE_BIT, true, false, portMAX_DELAY);
-        if (uxBits & CONNECTED_BIT)
-        {
-            ESP_LOGI(TAG, "WiFi Connected to ap");
-        }
-        if (uxBits & ESPTOUCH_DONE_BIT)
-        {
-            ESP_LOGI(TAG, "smartconfig over");
-            esp_smartconfig_stop();
-            vTaskDelete(NULL);
-        }
-    }
-}
-
-
-esp_event_handler_instance_t instance_any_id;
-
-esp_err_t wifi_event_handler(void *ctx, system_event_t *event)
-{
-    switch (event->event_id) {
-        case SYSTEM_EVENT_STA_START:
-            ESP_LOGI(TAG, "Wi-Fi STA started");
-            esp_wifi_connect();
-            break;
-        case SYSTEM_EVENT_STA_CONNECTED:
-            ESP_LOGI(TAG, "Wi-Fi STA connected");
-            break;
-        case SYSTEM_EVENT_STA_GOT_IP:
-            ESP_LOGI(TAG, "Wi-Fi connected, IP address: %s", ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
-            // 连接成功，执行您的逻辑代码
-            break;
-        // 其他Wi-Fi事件处理...
-        default:
-            break;
-    }
-
-    return ESP_OK;
-}
-void wifi()
-{
-    esp_netif_init();
-    esp_wifi_init(&wifi_init_config_t());
+    /* 定义WiFi连接的ssid和password参数 */
     wifi_config_t wifi_config = {
         .sta = {
-            .ssid = "MERCURY_9A5E",         // 替换为您的Wi-Fi网络的SSID
-            .password = "11111111", // 替换为您的Wi-Fi网络的密码
-            .scan_method = WIFI_ALL_CHANNEL_SCAN,
-            .sort_method = WIFI_CONNECT_AP_BY_SIGNAL,
-            .threshold.rssi = -127,
-            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+            .ssid     = MY_WIFI_SSID ,
+            .password = MY_WIFI_PASSWD
         },
     };
+    /* 设置WiFi的工作模式为 STA */
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    /* 设置WiFi连接的参数，主要是ssid和password */
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    /* 启动WiFi连接 */
+    ESP_ERROR_CHECK(esp_wifi_start());
 
-    esp_wifi_set_mode(WIFI_MODE_STA);
-    esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config);
-    esp_wifi_start();
+    printf("wifi_init_sta finished. \n");
 
+    /* 使用事件标志组等待连接建立(WIFI_CONNECTED_BIT)或连接失败(WIFI_FAIL_BIT)事件 */
+    EventBits_t bits;  /* 定义一个事件位变量来接收事件标志组等待函数的返回值 */
+    bits = xEventGroupWaitBits( wifi_event_group_handler,	        /* 需要等待的事件标志组的句柄 */
+                                WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,	/* 需要等待的事件位 */
+                                pdFALSE,    /* 为pdFALSE时，在退出此函数之前所设置的这些事件位不变，为pdFALSE则清零*/
+                                pdFALSE,    /* 为pdFALSE时，设置的这些事件位任意一个置1就会返回，为pdFALSE则需全为1才返回 */
+                                portMAX_DELAY);	                    /* 设置为最长阻塞等待时间，单位为时钟节拍 */
 
+    /* 根据事件标志组等待函数的返回值获取WiFi连接状态 */
+    if (bits & WIFI_CONNECTED_BIT)  /* WiFi连接成功事件 */
+	{
+        printf("connected to ap SSID:%s OK \n",MY_WIFI_SSID);
+        vEventGroupDelete(wifi_event_group_handler);    /* 删除WiFi连接事件标志组，WiFi连接成功后不再需要 */
+    }
+	else if (bits & WIFI_FAIL_BIT) /* WiFi连接失败事件 */
+	{
+        printf("Failed to connect to SSID:%s \n",MY_WIFI_SSID);
+        smartconfig_init_start();   /* 开启智能配网 */
+    }
+	else
+    {
+        printf("UNEXPECTED EVENT \n");  /* 没有等待到事件 */
+        smartconfig_init_start();   /* 开启智能配网 */
+    }
+
+    //ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler));
+    //ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler));
 }
+
+
 
 void app_main(void)
 {
@@ -497,18 +517,13 @@ void app_main(void)
     guiTask();            // lvgl显示接口初始化
     lv_port_indev_init(); // 按键接口
     ui_init();
-    esp_event_loop_create_default();
-    esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL, &instance_any_id);
-    while (true) {
-        if (wifi_is_connected()) {
-            ESP_LOGI(TAG, "Wi-Fi is connected");
-            // Wi-Fi连接成功，执行您的逻辑代码
-            break;
-        }
-        vTaskDelay(pdMS_TO_TICKS(1000));  // 每秒检查一次连接状态
-    }
-    // obtain_time();
-    // ESP_ERROR_CHECK( nvs_flash_init() );
+
+    ESP_ERROR_CHECK( nvs_flash_init() );
+    wifi_init_sta();
+    // 等待 Wi-Fi 连接成功
+
+    obtain_time();
+
     // initialise_wifi();
 
     // timer_ntp();
